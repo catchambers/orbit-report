3c0b37cdda8eb1363cf4acdfa982c9033e3d2db9
CAT CHAMBERS <caitlynlchambers@gmail.com>
2020-08-05 20:04:12 -0500

    Adding search button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Satellite } from './satellite';
+import { SelectorMatcher } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -12,6 +13,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbit-report';
   sourceList: Satellite[];
+  displayList: Satellite[];
   constructor() {
     // this.sourceList = [
     //    new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true),
@@ -20,6 +22,7 @@ export class AppComponent {
     //    new Satellite("GPS 938", "Positioning", "2001-11-01", "HIGH", true),
     //    new Satellite("ISS", "Space Station", "1998-11-20", "LOW", true),
     // ];
+      this.displayList = [];
       this.sourceList = [];
       let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
    
@@ -40,7 +43,22 @@ export class AppComponent {
                 );
               this.sourceList.push(satellite);
             };
+            // make a copy of the sourceList to be shown to the user
+            this.displayList = this.sourceList.slice(0);
          }.bind(this));
       }.bind(this));
+  }
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    // assign this.displayList to be the array of matching satellites
+    // this will cause Angular to re-make the table, but now only containing matches
+    this.displayList = matchingSatellites;
   }
 }
